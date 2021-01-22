860809dda897163f470d390bf88c1c68d91f9711
David Snyder <davidgraygs4@gmail.com>
2021-01-22 12:48:18 -0500

    finish up footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,88 @@
+import { H3, H4 } from "../shared/styles/headings";
+import Anchor from "../shared/ui-components/Anchor";
 import styles from "./index.styles";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Link from "next/link";
 
 const Footer = () => {
-	return <styles.footer></styles.footer>;
+	return (
+		<styles.footer>
+			<styles.top>
+				<styles.left>
+					<H3>The best Stream/Discord chat integration</H3>
+					<styles.h4>
+						DisStreamChat is the easiest way to link your Discord with your Stream chat
+					</styles.h4>
+					<styles.icons>
+						<Anchor href="https://github.com/DisStreamChat">
+							<GitHubIcon />
+						</Anchor>
+						<Anchor href="https://twitter.com/DisStreamChat">
+							<TwitterIcon />
+						</Anchor>
+					</styles.icons>
+				</styles.left>
+				<styles.right>
+					<styles.column>
+						<styles.heading>Products</styles.heading>
+						<Link href="/bot" passHref>
+							<Anchor>Use the Discord bot</Anchor>
+						</Link>
+						<Link href="/apps/download" passHref>
+							<Anchor>Get the Chat Client</Anchor>
+						</Link>
+						<Anchor
+							href="https://www.patreon.com/disstreamchat?fan_landing=true"
+							newTab
+						>
+							Support us on Patreon
+						</Anchor>
+					</styles.column>
+					<styles.column>
+						<styles.heading>Resources</styles.heading>
+						<Anchor
+							href="https://www.patreon.com/disstreamchat?fan_landing=true"
+							newTab
+						>
+							Join the Discord
+						</Anchor>
+						<Link href="/terms" passHref>
+							<Anchor>Terms of Service</Anchor>
+						</Link>
+						<Link href="/privacy" passHref>
+							<Anchor>Privacy Policy</Anchor>
+						</Link>
+					</styles.column>
+					<styles.column>
+						<styles.heading>Team</styles.heading>
+						<Link href="/members" passHref>
+							<Anchor>Members</Anchor>
+						</Link>
+
+						<Anchor href="https://github.com/DisStreamChat/Contributors" newTab>
+							Contributors
+						</Anchor>
+					</styles.column>
+				</styles.right>
+			</styles.top>
+			<styles.bottom>
+				<Anchor
+					href="https://github.com/DisStreamChat/Website/blob/master/LICENSE"
+					newTab
+					className="copyright"
+				>
+					© DisStreamChat 2020-2021
+				</Anchor>
+				<span className="made-by">
+					Made with ❤ by the{" "}
+					<Anchor href="https://github.com/orgs/DisStreamChat/people" newTab>
+						DisStreamChat Team
+					</Anchor>
+				</span>
+			</styles.bottom>
+		</styles.footer>
+	);
 };
 
 export default Footer;
---
+++ components/shared/styles/headings.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const H3 = styled.h3`
+	font-size: 1.17rem;
+	font-weight: bold;
+`
+
+export const H4 = styled.h4`
+	font-weight: 400;
+`
