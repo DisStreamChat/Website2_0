c9223de5674de8cc8481c6a86ca7db69c552151f
David Snyder <davidgraygs4@gmail.com>
2021-01-21 21:09:26 -0500

    remove unneeded boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import SEO from "../components/utils/SEO"
+
+Router.events.on("routeChangeStart", () => {
+	NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+	NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+	NProgress.done();
+});
+
+const App = ({ children }) => {
+	return <>{children}</>;
+};
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<App>
+			<SEO/>
+			<Component {...pageProps} />
+		</App>
+	);
+}
+
+export default MyApp;
