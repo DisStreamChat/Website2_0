c2914985580130c8514fdbef5e88e89a5bf0930e
David Snyder <davidgraygs4@gmail.com>
2021-01-22 23:36:56 -0500

    build hero section for landing page

--- components/shared/styles/headings.tsx
+++ components/shared/styles/headings.tsx
@@ -1,10 +1,16 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const H3 = styled.h3`
 	font-size: 1.17rem;
 	font-weight: bold;
-`
+`;
 
 export const H4 = styled.h4`
 	font-weight: 400;
-`
+`;
+
+export const H1 = styled.h1`
+	display: block;
+	font-size: 2em;
+	font-weight: bold;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,67 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Main } from "../components/shared/styles";
+import { H1, H3 } from "../components/shared/styles/headings";
+import { OrangeButton } from "../components/shared/ui-components/Button";
+
+const Hero = styled.section`
+	min-height: calc(100vh - 80px);
+	padding: 1rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 2rem;
+	box-sizing: content-box;
+	text-align: center;
+	color: white;
+	flex-direction: column;
+`;
+
+const Subheading = styled(H3)`
+	font-style: normal;
+	font-weight: 400;
+	font-size: 2rem;
+	line-height: 170%;
+	margin-top: 1rem;
+	text-align: center;
+	color: #aaa;
+	@media screen and (max-width: 425px){
+		font-size: 1.5rem;
+	}
+`;
+
+const Buttons = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	margin-top: 1rem;
+	max-width: 700px;
+	button{
+		margin: .5rem 2rem;
+		font-weight: bold;
+		font-size: 1.15rem;
+		@media screen and (max-width: 425px){
+			font-size: 1rem;
+		}
+	}
+`
 
 export default function Home() {
-
 	return (
-		<div>
-			<h1>Hello next</h1>
-		</div>
+		<Main>
+			<Hero>
+				<H1>Integrate your Discord server with Twitch</H1>
+				<Subheading>
+					Chat, moderation, interactivity, and much more easily Integrated with Twitch and
+					Discord!
+				</Subheading>
+				<Buttons>
+					<OrangeButton>See Features</OrangeButton>
+					<OrangeButton>Add to Discord</OrangeButton>
+					<OrangeButton>My Dashboard</OrangeButton>
+				</Buttons>
+			</Hero>
+		</Main>
 	);
 }
