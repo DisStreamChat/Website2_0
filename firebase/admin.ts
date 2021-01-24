91dc98815690f8d5d458937ee65d90ace8a290dd
David Snyder <davidgraygs4@gmail.com>
2021-01-24 18:27:39 -0500

    fix firebase admin service account

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin"
 
-const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, "base64").toString("ascii"));
+const serviceAccount = (require("../serviceAccount.json"));
 
 export const verifyIdToken = async (token) => {
 	if(!admin.apps.length){
