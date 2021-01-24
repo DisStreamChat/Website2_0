e9af82dc95ea6e6e213214204602d89cb9ff0227
David Snyder <davidgraygs4@gmail.com>
2021-01-24 10:26:41 -0500

    add in firebase

---
+++ firebase/admin.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin"
+
+const serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, "base64").toString("ascii"));
+
+export const verifyIdToken = async (token) => {
+	if(admin.apps.length){
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount),
+			databaseURL: "https://distwitchchat-db.firebaseio.com",
+		});
+	}
+
+	return admin.auth().verifyIdToken(token).catch(err => false)
+}
