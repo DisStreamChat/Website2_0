9b70b1f1280e16e2fe42ef7e11a4fbe642d36ad8
David Snyder <davidgraygs4@gmail.com>
2021-01-27 15:23:23 -0500

    add more to the discord dashboard

--- components/dashboard/Discord/Discord.tsx
+++ components/dashboard/Discord/Discord.tsx
@@ -7,17 +7,25 @@ const Description = styled.p`
 	font-weight: 400;
 	font-size: 1.17rem;
 	margin: 0.5rem 0;
+	opacity: 0.8;
 `;
 
 const ServerSelect = styled.div`
 	/* margin: 0.5rem 0; */
-	margin-bottom: .5rem;
+	margin-bottom: 0.5rem;
 `;
 
 const ServerTitle = styled(H2)`
 	margin-top: 0;
 	margin-bottom: 0.5rem;
+	text-transform: uppercase;
 `;
+
+const Title = styled(H2)`
+	margin: 0.5rem 0;
+`;
+
+const ServerArea = styled.div``;
 
 const Discord = () => {
 	return (
@@ -32,7 +40,13 @@ const Discord = () => {
 				<ServerTitle>Server</ServerTitle>
 				<StyledSelect placeholder="Select a Server" />
 			</ServerSelect>
-			<hr/>
+			<hr />
+			<ServerArea>
+				<Title>Command Prefix</Title>
+				<Description>
+					Set the prefix used to run DisStreamBot commands in this discord server
+				</Description>
+			</ServerArea>
 		</>
 	);
 };
