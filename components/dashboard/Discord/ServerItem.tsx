8d579a34b067b60e393f8d3633e49ee934c01243
David Snyder <davidgraygs4@gmail.com>
2021-01-27 20:00:06 -0500

    formatting

--- components/dashboard/Discord/ServerItem.tsx
+++ components/dashboard/Discord/ServerItem.tsx
@@ -2,9 +2,9 @@ import GuildIcon from "../../shared/styles/guildIcon";
 import { H3, H2 } from "../../shared/styles/headings";
 import { BlueButton } from "../../shared/ui-components/Button";
 import { ServerItemBody } from "./styles";
-import Link from "next/link"
+import Link from "next/link";
 import { useMediaQuery } from "@material-ui/core";
-
+import Anchor from "../../shared/ui-components/Anchor";
 
 interface ServerProps {
 	id: string;
@@ -14,8 +14,7 @@ interface ServerProps {
 }
 
 const ServerItem = ({ id, name, icon, botIn }: ServerProps) => {
-
-	const smallScreen = useMediaQuery("(max-width: 425px)")
+	const smallScreen = useMediaQuery("(max-width: 425px)");
 
 	return (
 		<ServerItemBody>
@@ -23,9 +22,15 @@ const ServerItem = ({ id, name, icon, botIn }: ServerProps) => {
 			<div>
 				<H2>{name}</H2>
 				<BlueButton tabIndex={-1}>
-					<Link href={`discord/${id}`}>
-						<a>Manage</a>
-					</Link>{" "}
+					{botIn ? (
+						<Link href={`discord/${id}`}>
+							<a>Manage</a>
+						</Link>
+					) : (
+						<Anchor href="https://invite.disstreamchat.com" newTab>
+							Invite
+						</Anchor>
+					)}
 				</BlueButton>
 			</div>
 		</ServerItemBody>
--- components/dashboard/Discord/ServerSelect.tsx
+++ components/dashboard/Discord/ServerSelect.tsx
@@ -18,7 +18,7 @@ const servers = [
 		id: "711238743213998091",
 		icon: "0abf1a3a68c1be1c4ccde1e208d1e2db",
 		name: "DisStreamChat Community",
-		botIn: true,
+		botIn: false,
 	},
 ];
 
