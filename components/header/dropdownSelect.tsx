de32e72cc55a5c2ddf979ce8fa032b5989cf7212
David Snyder <davidgraygs4@gmail.com>
2021-02-05 19:20:43 -0500

    change the main item of the dropdown to a button

--- components/header/dropdownSelect.tsx
+++ components/header/dropdownSelect.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import styles from "./index.styles";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import styled from "styled-components";
 
 interface dropdownProps {
 	title: string;
 }
 
+const DropDownMain = styled(styles.NavItem).attrs({ as: "button" })`
+	display: flex;
+	align-items: center;
+	flex-direction: row;
+`;
+
 const DropdownSelect = (props: dropdownProps) => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	return <styles.navItem>{props.title}</styles.navItem>;
+	return (
+		<DropDownMain>
+			{props.title} <KeyboardArrowDownIcon />
+		</DropDownMain>
+	);
 };
 
 export default DropdownSelect;
