f99c65c8ffeba0e422576b223e46686d5afa4f91
David Snyder <davidgraygs4@gmail.com>
2021-02-06 07:53:47 -0500

    add test utilities

---
+++ utils/test-utils.tsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React, { useMemo } from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const MaterialWrapper = ({ children }) => {
+	const theme = useMemo(
+		() =>
+			createMuiTheme({
+				palette: {
+					type: "dark",
+					primary: {
+						light: "#a6d4fa",
+						main: "#90caf9",
+						dark: "#648dae",
+						contrastText: "#ffffff",
+					},
+				},
+			}),
+		[]
+	);
+
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (ui, options) =>
+	render(ui, {
+		wrapper: MaterialWrapper,
+		...options,
+	});
+
+export * from "@testing-library/react"
+export {customRender as render} 
