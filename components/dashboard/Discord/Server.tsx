bc3115d780d98ce36fa97f36df19bb75f8f93c7e
David Snyder <davidgraygs4@gmail.com>
2021-02-06 11:11:58 -0500

    add conditional rendering to server component

--- components/dashboard/Discord/Server.tsx
+++ components/dashboard/Discord/Server.tsx
@@ -2,24 +2,39 @@ import React from "react";
 import PluginItem from "./PluginItem";
 import plugins from "../../../utils/plugins.json";
 import styled from "styled-components";
-
-
+import { useRouter } from "next/router";
+import Plugins from "./Plugins";
 
 const PluginBody = styled.div`
 	display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
-    gap: 25px;
-    -webkit-box-pack: center;
-    justify-content: center;
+	grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
+	gap: 25px;
+	-webkit-box-pack: center;
+	justify-content: center;
 `;
 
-const Server = ({serverId=""}) => {
+const Server = () => {
+	const router = useRouter();
+
+	const [, serverId, pluginName] = router.query.type as string[];
+
 	return (
-		<PluginBody>
-			{plugins.map(plugin => (
-				<PluginItem key={plugin.id} serverId={serverId} {...plugin} active={false} />
-			))}
-		</PluginBody>
+		<>
+			{!pluginName ? (
+				<PluginBody>
+					{plugins.map(plugin => (
+						<PluginItem
+							key={plugin.id}
+							serverId={serverId}
+							{...plugin}
+							active={false}
+						/>
+					))}
+				</PluginBody>
+			) : (
+				<Plugins />
+			)}
+		</>
 	);
 };
 
