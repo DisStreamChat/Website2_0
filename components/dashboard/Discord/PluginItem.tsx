b21766613da0b86051c1163faf50989f9fe719a2
David Snyder <davidgraygs4@gmail.com>
2021-02-05 21:06:02 -0500

    improve style on the plugin items

--- components/dashboard/Discord/PluginItem.tsx
+++ components/dashboard/Discord/PluginItem.tsx
@@ -22,16 +22,48 @@ const PluginCard = styled.div`
 	min-width: 350px;
 	min-height: 120px;
 	padding: 20px;
-	align-items: center;
+	/* align-items: center; */
 	box-shadow: 3px 3px 5px 0 #111;
+	& > * + * {
+		margin-left: 0.75rem;
+	}
+	@supports (gap: 10px) {
+		gap: 0.75rem;
+		& > * + * {
+			margin-left: 0;
+		}
+	}
+	& span:last-child,
+	div:first-child {
+		align-self: start;
+	}
+`;
+
+const PluginTitle = styled.div`
+	font-size: 1.25rem;
+	font-weight: bold;
+	text-transform: uppercase;
+`;
+
+const PluginBody = styled.div`
+	font-weight: 400;
+	color: #aaa;
+	margin-top: 0.25rem;
+	line-height: 1.5rem;
 `;
 
 const PluginItem = (props: pluginProps) => {
-
-	const [checked, setChecked] = useState(props.active)
+	const [checked, setChecked] = useState(props.active);
 
 	return (
 		<PluginCard>
+			<div>
+				<img src={`/${props.image}`} alt="" width={50} height={50} />
+			</div>
+			<div>
+				<PluginTitle>{props.title}</PluginTitle>
+				<PluginBody>{props.description}</PluginBody>
+			</div>
 			<Switch
 				checked={checked}
 				onChange={e => setChecked(e.target.checked)}
