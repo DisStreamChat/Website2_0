5a8527a564c517818af833b357090bd838f02854
David Snyder <davidgraygs4@gmail.com>
2021-02-11 06:49:53 -0500

    start adding in the leveling plugin page

--- components/dashboard/Discord/Plugins.tsx
+++ components/dashboard/Discord/Plugins.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
+import Leveling from "./pluginPages/leveling";
+
 const Plugins = () => {
-	return <></>;
+	const router = useRouter();
+
+	const [, serverId, pluginName] = router.query.type as string[];
+
+	switch (pluginName) {
+		case "leveling":
+			return <Leveling />;
+		default:
+			return <></>;
+	}
 };
 
 export default Plugins;
---
+++ components/dashboard/Discord/pluginPages/leveling.tsx
@@ -0,0 +1,5 @@
+const Leveling = () => {
+	return <>leveling</>;
+};
+
+export default Leveling;
