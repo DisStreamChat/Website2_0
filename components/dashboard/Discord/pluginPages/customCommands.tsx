8ebb94bdd942fe8a73f7ef9a0a6bfd34e9670ef1
David Snyder <davidgraygs4@gmail.com>
2021-03-03 21:35:00 -0500

    setup default values and editing for custom commands

--- components/dashboard/Discord/pluginPages/customCommands.tsx
+++ components/dashboard/Discord/pluginPages/customCommands.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import styled from "styled-components";
-import { H2, H3 } from "../../../shared/styles/headings";
+import { H1, H2, H3 } from "../../../shared/styles/headings";
 import { BlueButton } from "../../../shared/ui-components/Button";
 import { PluginSubHeader } from "./styles";
 import { useDocumentData } from "react-firebase-hooks/firestore";
@@ -10,6 +10,8 @@ import ListItem from "../../../shared/ui-components/ListItem";
 import { isEqual } from "lodash";
 import SaveBar from "../../../shared/ui-components/SaveBar";
 import Modal from "../../../shared/ui-components/Modal";
+import { command, commandMap } from "../../../../utils/types";
+import ClearIcon from "@material-ui/icons/Clear";
 
 const CommandsHeader = styled(PluginSubHeader)`
 	display: flex;
@@ -23,26 +25,6 @@ const CommandsHeader = styled(PluginSubHeader)`
 	}
 `;
 
-export interface command {
-	DM?: boolean;
-	allowedChannels: string[];
-	bannedRoles: string[];
-	cooldown: number;
-	cooldownTime: number;
-	deleteUsage?: boolean;
-	description: string;
-	lastUsed: number;
-	message: string;
-	name: string;
-	permittedRoles: string[];
-	role: false;
-	type: string;
-}
-
-interface commandMap {
-	[key: string]: command;
-}
-
 const CommandModalBody = styled.div`
 	width: 100vw;
 	height: 100vh;
@@ -52,24 +34,88 @@ const CommandModalBody = styled.div`
 const actions = {
 	UPDATE: "update",
 	SET: "set",
-	CLEAR: "clear"
-}
+	CLEAR: "clear",
+	RESET: "clear"
+};
+
+const defaultCommand = serverId => ({
+	DM: false,
+	allowedChannels: [],
+	bannedRoles: [],
+	cooldown: 0,
+	cooldownTime: 0,
+	deleteUsage: false,
+	description: "",
+	lastUsed: 0,
+	message: "",
+	name: "",
+	permittedRoles: [serverId],
+	role: false,
+	type: "text",
+});
 
 const commandReducer = (state, action) => {
-	switch(action.type){
+	switch (action.type) {
 		case actions.UPDATE:
-			return {...state, [action.key]: action.value}
+			return { ...state, [action.key]: action.value };
 		case actions.SET:
-			return action.value
-		case actions.CLEAR: 
-			return {}
+			return action.value;
+		case actions.CLEAR:
+		case actions.RESET:
+			return defaultCommand(action.server);
 	}
-}
+};
 
-const CommandModal = props => {
+const CommandHeader = styled.div`
+	box-sizing: border-box !important;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0.5rem 1.5rem;
+	width: 100vw;
+	height: 70px;
+	box-shadow: 0 1px 0 0 hsl(0deg 0% 67% / 20%), 0 1px 2px 0 hsl(0deg 0% 67% / 20%);
+	button {
+		background: #101010;
+		color: #aaa;
+		border-radius: 0.25rem;
+		border: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 36px;
+		outline: none !important;
+	}
+`;
+
+const CommandModal = ({ defaultValue, ...props }) => {
+	const router = useRouter();
+	const [, serverId, pluginName] = router.query.type as string[];
+
+	const [state, dispatch] = useReducer(commandReducer, defaultValue ?? defaultCommand(serverId));
+
+	useEffect(() => {
+		if (defaultValue) {
+			console.log("editing a command")
+			dispatch({ type: actions.SET, value: { ...defaultValue } });
+		}else{
+			dispatch({type: actions.RESET, server: serverId})
+		}
+	}, [defaultValue]);
+
 	return (
 		<Modal open={props.open} onClose={props.onClose}>
-			<CommandModalBody></CommandModalBody>
+			<CommandModalBody>
+				<CommandHeader>
+					<H2>Create Text Command</H2>
+					<button onClick={props.onClose}>
+						<ClearIcon />
+					</button>
+				</CommandHeader>
+				<pre>
+					{JSON.stringify(state, null, 4)}
+				</pre>
+			</CommandModalBody>
 		</Modal>
 	);
 };
@@ -80,16 +126,14 @@ const CustomCommands = () => {
 	const [, serverId, pluginName] = router.query.type as string[];
 	const [localCommands, setLocalCommands] = useState<commandMap>({});
 	const [commandModalOpen, setCommandModalOpen] = useState(false);
-
 	const collectionRef = firebaseClient.db.collection("customCommands");
-
 	const [snapshot, loading, error] = useDocumentData(collectionRef.doc(serverId));
+	const [commandBeingEdited, setCommandBeingEdited] = useState<command | null>(null);
 
 	useEffect(() => {
 		if (!snapshot) {
 			collectionRef.doc(serverId).set({}, { merge: true });
 		} else {
-			console.log("setting local commands");
 			setLocalCommands(snapshot);
 		}
 	}, [snapshot]);
@@ -112,11 +156,22 @@ const CustomCommands = () => {
 		collectionRef.doc(serverId).set(localCommands);
 	};
 
+	const createCommand = () => {
+		setCommandBeingEdited(null);
+		setCommandModalOpen(true);
+	};
+
+	const edit = command => {
+		setCommandBeingEdited(command)
+		setCommandModalOpen(true)
+	}
+
 	return (
 		<div>
 			<CommandModal
 				open={commandModalOpen}
 				onClose={() => setCommandModalOpen(false)}
+				defaultValue={commandBeingEdited}
 			></CommandModal>
 			<CommandsHeader>
 				<span>
@@ -124,7 +179,7 @@ const CustomCommands = () => {
 					<h4>A simple command that responds with a custom message in DM or public</h4>
 				</span>
 				<span>
-					<BlueButton onClick={() => setCommandModalOpen(true)}>Create Command</BlueButton>
+					<BlueButton onClick={createCommand}>Create Command</BlueButton>
 				</span>
 			</CommandsHeader>
 			<span>
@@ -132,7 +187,7 @@ const CustomCommands = () => {
 				<ul>
 					{snapshot &&
 						commands.map(([key, val]) => (
-							<ListItem delete={() => deleteMe(key)} edit={() => {}}>
+							<ListItem delete={() => deleteMe(key)} edit={() => edit(val)}>
 								<div>
 									<img src="/speech.svg" alt="" width="50" />
 								</div>
--- utils/types.tsx
+++ utils/types.tsx
@@ -18,3 +18,23 @@ export interface channel {
 export interface server {
 
 }
+
+export interface command {
+	DM?: boolean;
+	allowedChannels: string[];
+	bannedRoles: string[];
+	cooldown: number;
+	cooldownTime: number;
+	deleteUsage?: boolean;
+	description: string;
+	lastUsed: number;
+	message: string;
+	name: string;
+	permittedRoles: string[];
+	role: false;
+	type: string;
+}
+
+export interface commandMap {
+	[key: string]: command;
+}
