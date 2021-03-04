a16d696775829e52e3c9932960876bd77a5474d4
David Snyder <davidgraygs4@gmail.com>
2021-03-03 21:12:34 -0500

    added a file for global types

---
+++ utils/types.tsx
@@ -0,0 +1,20 @@
+export interface role {
+	id: string,
+	name: string,
+	color: string,
+	rawPosition: number,
+	position: number,
+	permissions: string[]
+}
+
+export interface guildMember {
+
+}
+
+export interface channel {
+
+}
+
+export interface server {
+
+}
