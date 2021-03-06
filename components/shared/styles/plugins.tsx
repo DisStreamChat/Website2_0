10885fc37287cdcd34011354b2f6382a678924dc
David Snyder <davidgraygs4@gmail.com>
2021-03-06 11:42:37 -0500

    moved some styles to a seperate file

---
+++ components/shared/styles/plugins.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SectionTitle = styled.div`
+	color: white;
+	text-transform: uppercase;
+	padding: 0;
+	font-size: 12px;
+	font-weight: 600;
+	margin-bottom: 8px;
+`;
+
+export const SectionSubtitle = styled.div`
+	color: #aaa;
+	margin-bottom: 8px;
+	font-size: 14px;
+`;
