c6b34d4fe0d61c7542991f757c2108a91184376e
David Snyder <davidgraygs4@gmail.com>
2021-05-06 07:11:43 -0400

    fix typos

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -63,4 +63,4 @@ export const colorStyles = {
 	singleValue: styles => ({ ...styles, color: "white" }),
 };
 
-export const redirect_uri = process.env.NODE_ENV === "production" ? "https://dev.disstreamchat.com/oauth" : "http://localhost:3000/oauth" 
+export const redirect_uri = process.env.NODE_ENV === "production" ? "https://www.disstreamchat.com/oauth" : "http://localhost:3000/oauth" 
